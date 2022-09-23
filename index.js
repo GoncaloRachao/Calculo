df2cb85164040443ff51fef162ed2144c5bbaac9
GoncaloRachao <113585986+GoncaloRachao@users.noreply.github.com>
2022-09-23 11:17:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function multiplicapor()
+{
+let num1=document.getElementById("numeroum").value;
+let num2=document.getElementById("numerodois").value;
+document.getElementById("resultado").innerHTML = num1 *num2;
+
+}
+
+function dividepor()
+{
+let num1=document.getElementById("numeroum").value;
+let num2=document.getElementById("numerodois").value;
+document.getElementById("resultado").innerHTML = num1 / num2;
+
+}
